0ee805ac4b4ad733766a91827d78e77f2955d2f0
Uma Petersen <Uma@Umas-MacBook-Pro.local>
2016-04-07 21:30:30 -0700

    created methods for security

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -1,3 +1,37 @@
-import { Mongo } from 'meteor/mongo';
- 
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo'; 
+import { check } from 'meteor/check';
+
+
 export const Tasks = new Mongo.Collection('tasks');
+
+Meteor.methods({
+	'task.insert'(text) {
+		check(text, String);
+		// Make sure the user is logged in before inserting task 
+
+		if (! Meteor.userId()) {
+			throw new Meteor.Error('not-authorized');
+
+		}
+
+		Tasks.insert({
+			text,
+			createdAt: new Date(),
+			owner: Meteor.userId(),
+			username: Meteor.user().username,
+		});
+	},
+	'tasks.remove'(taskId) {
+		check(taskId, String);
+
+		Tasks.remove(taskId);
+	},
+	'task.setChecked'(taskId, setChecked) {
+		check(taskId, String);
+		check(setChecked, Boolean);
+
+		Tasks.update(taskId, { $set: { checked: setChecked } });
+	},
+});
+
